d4f6c276c0525be8b968d31808436d9515a4645e
lobsterwise <pangolin630@gmail.com>
2023-02-08 13:04:48 -0500

    Add dummy subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,120 +1,148 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 1892-1893 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
-import java.io.FileNotFoundException;
 import java.util.Arrays;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import frc.robot.subsystems.SubsystemManager;
 import frc.lib.loops.Looper;
-import frc.lib.util.ReflectingLogger;
+import frc.lib.models.DriveTrajectoryGenerator;
+import frc.lib.statemachine.StateMachine;
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.Dummy.State;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  private SubsystemManager manager;
-  private Looper enabledLooper;
-  private Looper disabledLooper;
+    private SubsystemManager manager;
+    private Looper enabledLooper, disabledLooper;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    manager = new SubsystemManager(Arrays.asList(), false);
-    enabledLooper = new Looper();
-    disabledLooper = new Looper();
-    // Register the looper threads to the manager to use for enabled and disabled
-    manager.registerEnabledLoops(enabledLooper);
-    manager.registerDisabledLoops(disabledLooper);
 
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        manager = new SubsystemManager(
+            Arrays.asList(
+                Dummy.getInstance()
+            ),
+            true
+        );
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    manager.outputTelemetry();
-  }
+        // create the master looper threads
+        DriveTrajectoryGenerator.getInstance();
+        enabledLooper = new Looper();
+        disabledLooper = new Looper();
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    disabledLooper.stop();
-    // Reset subsystems here
-    enabledLooper.start();
-  }
+        // register the looper threads to the manager to use for enabled and disabled
+        manager.registerEnabledLoops(enabledLooper);
+        manager.registerDisabledLoops(disabledLooper);
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
+        // add any additional logging sources for capture
+        manager.addLoggingSource(Arrays.asList(StateMachine.getInstance()));
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {
-    disabledLooper.stop();
-    // Reset subsystems here
-    enabledLooper.start();
-  }
+        initButtons();
+    }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
+    /**
+     * This function is called every robot packet, no matter the mode. Use
+     * this for items like diagnostics that you want ran during disabled,
+     * autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before
+     * LiveWindow and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        manager.outputTelemetry();
+    }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {
-    enabledLooper.stop();
-    // Reset subsystems here
-    disabledLooper.start();
-  }
+    @Override
+    public void disabledInit() {
+        enabledLooper.stop();
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+        StateMachine.getInstance().assertStop();
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {
-    disabledLooper.stop();
-    // Reset subsystems here
-    enabledLooper.start();
-  }
+        disabledLooper.start();
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional comparisons to
+     * the switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        disabledLooper.stop();
+
+        //reset anything here
+        enabledLooper.start();
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        
+    }
+
+    @Override
+    public void teleopInit() {
+        disabledLooper.stop();
+
+        //reset anything here
+        Dummy.getInstance().state = State.kStopped;
+        //Constants.WHEELS = SmartDashboard.getBoolean("Drive/Wheel Control", Constants.WHEELS);
+        initButtons();
+        enabledLooper.start();
+        
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        
+    }
+
+    @Override
+    public void testInit() {
+        disabledLooper.stop();
+
+        //reset anything here
+        Dummy.getInstance().reset();
+
+        enabledLooper.start();
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+
+    }
+
+    public void initButtons(){
+        // no buttons for you
+    }
 }
--- src/main/java/frc/robot/subsystems/Input.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems;
-
-public class Input extends Subsystem {
-	
-}
