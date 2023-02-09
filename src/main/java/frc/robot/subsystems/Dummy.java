c321a844661379b98e74decdceae0b2b09269837
A-Worbot <34491601+PhillipWhiter@users.noreply.github.com>
2023-02-09 18:15:02 -0500

    Add logging (Raleigh)

--- src/main/java/frc/robot/subsystems/Dummy.java
+++ src/main/java/frc/robot/subsystems/Dummy.java
@@ -1,19 +1,17 @@
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 //design pattern for caching periodic writes to avoid hammering the HAL/CAN.
 public class Dummy extends Subsystem {
 	private static Dummy instance = new Dummy();
 	public static Dummy getInstance() { return instance; }
+	private DummyIO periodic;
 
 	public enum State {
 		kRunning,
 		kStopped
 	}
 	public State state;
-
-	private int count;
 
 	public Dummy() {
 		state = State.kStopped;
@@ -28,18 +26,26 @@ public class Dummy extends Subsystem {
 	 * Writes the periodic outputs to actuators (motors and ect...)
 	 */
 	public void writePeriodicOutputs() {
-		count++;
+		periodic.count++;
 	}
 
 	/**
 	 * Outputs all logging information to the SmartDashboard
 	 */
 	public void outputTelemetry() {
-		SmartDashboard.putNumber("Dummy count", count);
+		SmartDashboard.putNumber("Dummy count", periodic.count);
 	}
 
 	/**
 	 * Called to reset and configure the subsystem
 	 */
 	public void reset() {}
+
+	public class DummyIO extends PeriodicIO {
+		public int count = 0;
+	}
+
+	public LogData getLogger() {
+		return periodic;
+	}
 }
