b9d52d8e2a79241311313e283184684a77e177d8
A-Worbot <worbots10@gmail.com>
2023-02-16 17:57:40 -0500

    tested intake

--- src/main/java/frc/robot/subsystems/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/SuperStructure.java
@@ -64,15 +64,15 @@ public class SuperStructure extends Subsystem {
 	}
 
 	public void writePeriodicOutputs() {
-		leftSideWheel.set(ControlMode.PercentOutput, 1 * Constants.SIDE_WHEELS_MULTIPLIER );
-		rightSideWheel.set(ControlMode.PercentOutput, 1 * Constants.SIDE_WHEELS_MULTIPLIER);
-		conveyorBelt.set(ControlMode.PercentOutput, 1 * Constants.CONVEYER_BELT_MULTIPLIER);
-		intakeWheelSpinner.set(ControlMode.PercentOutput, 1 * Constants.INTAKE_WHEEL_SPINNER_MULTIPLIER);
+		// leftSideWheel.set(ControlMode.PercentOutput, 1 * Constants.SIDE_WHEELS_MULTIPLIER );
+		// rightSideWheel.set(ControlMode.PercentOutput, 1 * Constants.SIDE_WHEELS_MULTIPLIER);
+		// conveyorBelt.set(ControlMode.PercentOutput, 1 * Constants.CONVEYER_BELT_MULTIPLIER);
+		// intakeWheelSpinner.set(ControlMode.PercentOutput, 1 * Constants.INTAKE_WHEEL_SPINNER_MULTIPLIER);
 		
-		// leftSideWheel.set(ControlMode.PercentOutput, periodic.power * Constants.SIDE_WHEELS_MULTIPLIER );
-		// rightSideWheel.set(ControlMode.PercentOutput, periodic.power * Constants.SIDE_WHEELS_MULTIPLIER);
-		// conveyorBelt.set(ControlMode.PercentOutput, periodic.power * Constants.CONVEYER_BELT_MULTIPLIER);
-		// intakeWheelSpinner.set(ControlMode.PercentOutput, periodic.power * Constants.INTAKE_WHEEL_SPINNER_MULTIPLIER);
+		leftSideWheel.set(ControlMode.PercentOutput, periodic.power * Constants.SIDE_WHEELS_MULTIPLIER );
+		rightSideWheel.set(ControlMode.PercentOutput, periodic.power * Constants.SIDE_WHEELS_MULTIPLIER);
+		conveyorBelt.set(ControlMode.PercentOutput, periodic.power * Constants.CONVEYER_BELT_MULTIPLIER);
+		intakeWheelSpinner.set(ControlMode.PercentOutput, periodic.power * Constants.INTAKE_WHEEL_SPINNER_MULTIPLIER);
 		switch (periodic.intakePosition) {
 			case kUp:
 				intakeSolenoid.set(Value.kForward);
