25fc2d06602808314800b9a9d4263e3a6e6e0c35
A-Worbot <worbots10@gmail.com>
2023-02-25 14:09:46 -0500

    Update PivotMoveAction.java

--- src/main/java/frc/robot/actions/arm/PivotMoveAction.java
+++ src/main/java/frc/robot/actions/arm/PivotMoveAction.java
@@ -1,4 +1,19 @@
 package frc.robot.actions.arm;
+import frc.lib.statemachine.Action;
+import frc.robot.subsystems.Arm;
+
+public class PivotMoveAction extends Action {
+	
+	double power;
+	public PivotMoveAction(double power) {
+		this.power = power;
+	}
+
+	@Override
+	public void onStart() {
+		Arm.getInstance().setRawPivotPower(power);
+	}
+
 	@Override
 	public void onLoop() {}
 
