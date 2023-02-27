55c69f30e71e2c5208e5644225453c73aeafa54d
A-Worbot <worbots10@gmail.com>
2023-02-27 17:16:40 -0500

    Add getting angle from arm

--- src/main/java/frc/robot/subsystems/VisionLink.java
+++ src/main/java/frc/robot/subsystems/VisionLink.java
@@ -12,30 +12,25 @@ public class VisionLink extends Subsystem {
 	}
 
 	public void readPeriodicInputs() {
+		// periodic.turretAngle = Arm.getInstance().getTurretAngle();
         //need to get turret angle from arm subsystem
         //also read smartdashboard for vision values
     }
 
 	public void writePeriodicOutputs() {
-        if (periodic.turretAngle >= 180) {
-            periodic.turretAngle -= 0.1;
-        } if (periodic.turretAngle <= 0) {
-            periodic.turretAngle += 0.1;
-        }
 	}
 
     //add getters for vision values, so other subsystems dont need to get values from smartdash
 
 	public void outputTelemetry() {
-		SmartDashboard.putNumber("VisionData", periodic.turretAngle);
+		SmartDashboard.putNumber("VisionData/TurretAngle", periodic.turretAngle);
 	}
 
 	public void reset() {
-        periodic.turretAngle = 0.0;
     }
 
 	public class VisionIO extends PeriodicIO {
-		public double turretAngle = 0;
+		public double turretAngle;
 	}
 
 	public LogData getLogger() {
