5a36411f100b2769bbba991287b04602b6b38f5b
A-Worbot <worbots10@gmail.com>
2023-03-04 14:33:28 -0500

    added disable limit switch action

---
+++ src/main/java/frc/robot/actions/arm/DisableLimitSwitchAction.java
@@ -0,0 +1,32 @@
+package frc.robot.actions.arm;
+
+import frc.lib.statemachine.Action;
+import frc.robot.subsystems.Arm;
+
+public class DisableLimitSwitchAction extends Action{
+
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onLoop() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void onStop() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
